38fd53c2161edd3d1c103b9b863c0f6d18372f0f
cosmin-oros <98512765+cosmin-oros@users.noreply.github.com>
2022-03-13 17:40:12 +0200

    Add files via upload

---
+++ java-projects-problems/draganddrop/MyFrame.java
@@ -0,0 +1,19 @@
+package DragAndDrop;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+    DragPanel dragPanel = new DragPanel();
+
+
+    MyFrame(){
+        this.add(dragPanel);
+
+        this.setTitle("Drag & Drop demo");
+        this.setSize(600,600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+    }
+}
