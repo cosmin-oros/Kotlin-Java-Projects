f7c290e6b44e652e2dfbfedcf11ce7c79294b99c
Oros Cosmin-Cristian <98512765+cosmin-oros@users.noreply.github.com>
2022-04-26 20:23:05 +0300

    Add files via upload

---
+++ kotlin-projects-problems/random-problems/DwellingAbsClass.kt
@@ -0,0 +1,51 @@
+fun main() {
+    val squareCabin = SquareCabin(6)
+    val roundHut = RoundHut(3)
+    val roundTower = RoundTower(4)
+
+    with(squareCabin) {
+        println("\nSquare Cabin\n============")
+        println("Capacity: ${capacity}")
+        println("Material: ${buildingMaterial}")
+        println("Has room? ${hasRoom()}")
+    }
+
+    with(roundHut) {
+        println("\nRound Hut\n=========")
+        println("Material: ${buildingMaterial}")
+        println("Capacity: ${capacity}")
+        println("Has room? ${hasRoom()}")
+    }
+
+    with(roundTower) {
+        println("\nRound Tower\n==========")
+        println("Material: ${buildingMaterial}")
+        println("Capacity: ${capacity}")
+        println("Has room? ${hasRoom()}")
+    }
+}
+
+
+abstract class Dwelling(private var residents: Int) {
+    abstract val buildingMaterial: String
+    abstract val capacity: Int
+
+    fun hasRoom(): Boolean {
+        return residents < capacity
+    }
+}
+
+class SquareCabin(residents: Int) : Dwelling(residents) {
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
+}
+
+open class RoundHut(residents: Int) : Dwelling(residents) {
+    override val buildingMaterial = "Straw"
+    override val capacity = 4
+}
+
+class RoundTower(residents: Int) : RoundHut(residents) {
+    override val buildingMaterial = "Stone"
+    override val capacity = 4
+}
