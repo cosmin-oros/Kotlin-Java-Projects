e40050f8492a829c6d6b32cb896e50e95f79c14a
Oros Cosmin-Cristian <98512765+cosmin-oros@users.noreply.github.com>
2022-05-04 18:27:20 +0300

    Add files via upload

---
+++ kotlin-projects-problems/random-problems/Lists.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6)
+    println("List: $numbers")
+    println("Size: ${numbers.size}")
+
+    // Access elements of the list
+    println("First element: ${numbers[0]}")
+    println("Second element: ${numbers[1]}")
+    println("Last index: ${numbers.size - 1}")
+    println("Last element: ${numbers[numbers.size - 1]}")
+    println("First: ${numbers.first()}")
+    println("Last: ${numbers.last()}")
+
+    // Use the contains() method
+    println("Contains 4? ${numbers.contains(4)}")
+    println("Contains 7? ${numbers.contains(7)}")
+}
