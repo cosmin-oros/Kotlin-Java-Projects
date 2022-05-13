ae0baf51e0b934336860d9a2d1517c2dc88ee702
Oros Cosmin-Cristian <98512765+cosmin-oros@users.noreply.github.com>
2022-05-13 11:47:19 +0300

    Add files via upload

---
+++ kotlin-projects-problems/random-problems/WordLists.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val words = listOf("about", "acute", "awesome", "balloon", "best", "brief", "class", "coffee", "creative")
+    val filteredWords = words.filter { it.startsWith("b", ignoreCase = true) }
+        .shuffled()
+        .take(2)
+        .sorted()
+    println(filteredWords)
+}
