9482a8373df20035a25642b89fc778027d34a270
Oros Cosmin-Cristian <98512765+cosmin-oros@users.noreply.github.com>
2022-05-13 11:42:40 +0300

    Add files via upload

---
+++ kotlin-projects-problems/random-problems/Collections.kt
@@ -0,0 +1,12 @@
+
+fun main() {
+    val numbers = listOf(0, 3, 8, 4, 0, 5, 5, 8, 9, 2)
+    println("list:   ${numbers}")
+    println("sorted: ${numbers.sorted()}")
+    val setOfNumbers = numbers.toSet()
+    println("set:    ${setOfNumbers}")
+    val set1 = setOf(1,2,3)
+    val set2 = mutableSetOf(3,2,1)
+    println("$set1 == $set2: ${set1 == set2}")
+    println("contains 7: ${setOfNumbers.contains(7)}")
+}
