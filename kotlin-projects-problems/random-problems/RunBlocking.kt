e88945a874443e6baad480fd742d70242fbf308e
Oros Cosmin-Cristian <98512765+cosmin-oros@users.noreply.github.com>
2022-06-13 12:56:15 +0300

    Add files via upload

---
+++ kotlin-projects-problems/random-problems/RunBlocking.kt
@@ -0,0 +1,21 @@
+import kotlinx.coroutines.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+val formatter = DateTimeFormatter.ISO_LOCAL_TIME
+val time = { formatter.format(LocalDateTime.now()) }
+
+suspend fun getValue(): Double{
+    println("Entering getValue() at ${time()}")
+    delay(3000)
+    println("leaving getValue() at ${time()}")
+    return Math.random()
+}
+
+fun main() {
+    runBlocking {
+        val num1 = async { getValue() }
+        val num2 = async { getValue() }
+        println("result of num1 + num2 is ${num1.await() + num2.await()}")
+    }
+}
