0aa2558f7d82c29f754eb492c3644024ac0617c1
Oros Cosmin-Cristian <98512765+cosmin-oros@users.noreply.github.com>
2022-06-13 12:59:09 +0300

    Add files via upload

---
+++ kotlin-projects-problems/random-problems/Coroutines.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
+    repeat(3) {
+        Thread {
+            println("${Thread.currentThread()} has started")
+            for (i in states) {
+                println("${Thread.currentThread()} - $i")
+                Thread.sleep(50)
+            }
+        }.start()
+    }
+}
+
+/*
+
+import kotlinx.coroutines.*
+
+fun main() {
+   val states = arrayOf("Starting", "Doing Task 1", "Doing Task 2", "Ending")
+   repeat(3) {
+       GlobalScope.launch {
+           println("${Thread.currentThread()} has started")
+           for (i in states) {
+               println("${Thread.currentThread()} - $i")
+           }
+       }
+   }
+}
+
+ */
