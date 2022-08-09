a236523856dfb345e988d7acd8c45084b76a76e0
cosmin-oros <oroscosmin09@gmail.com>
2022-08-09 11:44:40 +0300

    two sum problem

---
+++ java-projects-problems/Problems/TwoSum/TwoSumProblem.java
@@ -0,0 +1,10 @@
+package Problems.TwoSum;
+
+public class TwoSumProblem {
+    public static void main(String args[]){
+        TwoSum sum = new TwoSum();
+        int[] nrs = {1, 2, 3, 4, 5};
+        int target = 5;
+        System.out.println(sum.twoSum(nrs, target));
+    }
+}
