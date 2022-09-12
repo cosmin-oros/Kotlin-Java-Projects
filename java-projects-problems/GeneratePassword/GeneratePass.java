3cf5777583ede11fe442b2b273d3f8a27a69b0a6
cosmin-oros <oroscosmin09@gmail.com>
2022-09-12 15:49:34 +0300

    password generator

---
+++ java-projects-problems/GeneratePassword/GeneratePass.java
@@ -0,0 +1,44 @@
+package GeneratePassword;
+
+import java.util.Random;
+
+public class GeneratePass {
+    public static void main(String[] args)
+    {
+        int length = 10;
+        System.out.println(password(length));
+    }
+
+    static char[] password(int len)
+    {
+        System.out.println("Generating password using random() : ");
+        System.out.print("Your new password is : ");
+
+        // A strong password has Cap_chars, Lower_chars,
+        // numeric value and symbols. So we are using all of
+        // them to generate our password
+        String Capital_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String Small_chars = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "0123456789";
+        String symbols = "!@#$%^&*_=+-/.?<>)";
+
+
+        String values = Capital_chars + Small_chars +
+                numbers + symbols;
+
+        // Using random method
+        Random rndm_method = new Random();
+
+        char[] password = new char[len];
+
+        for (int i = 0; i < len; i++)
+        {
+            // Use of charAt() method : to get character value
+            // Use of nextInt() as it is scanning the value as int
+            password[i] = values.charAt(rndm_method.nextInt(values.length()));
+
+        }
+        
+        return password;
+    }
+}
